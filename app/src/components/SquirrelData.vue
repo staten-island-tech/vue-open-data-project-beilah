cffd165c85489e0c0578907d0bba3ca38be273b6
Beila <beilah@nycstudents.net>
2026-03-17 14:03:45 -0400

    asdasd

---
+++ app/src/components/SquirrelData.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2></h2>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+    squirrel: {
+        type: Object,
+        required: true,
+    },
+    id: {
+        type: Number,
+        required: true,
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/views/Squirrel.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <h1>bye</h1>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import SquirrelData from '@/components/SquirrelData.vue'
+const squirrel = ref([])
+async function getSquirrel() {
+    try {
+        const response = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+        const data = await response.json()
+        squirrel.value = data.results
+    } catch (error) {
+        console.log(error)
+    }
+}
+onMounted(() => {
+    getSquirrel()
+})
+</script>
+
+<style scoped>
+
+</style>
