a5621a3d118e667b3be5304d411b66837bc8d9a0
Beila <beilah@nycstudents.net>
2026-03-26 13:34:01 -0400

    Update Squirrel.vue

--- app/src/views/Squirrel.vue
+++ app/src/views/Squirrel.vue
@@ -18,25 +18,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import SquirrelCard from '@/components/SquirrelCard.vue'
-
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  data() {
-    return {
-      chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true
-      }
-    }
-  }
-}
-  
 
 const squirrels = ref([])
 
