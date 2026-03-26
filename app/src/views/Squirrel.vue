698f45013b569553e2df9e08d0378a1739b2d0ca
Beila <beilah@nycstudents.net>
2026-03-26 14:03:50 -0400

    Update Squirrel.vue

--- app/src/views/Squirrel.vue
+++ app/src/views/Squirrel.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="container">
     <h1>NYC Squirrels</h1>
+    <div class="chart-wrapper">
+      <Radar id="radar" :options="chartOptions" :data="radarData" />
+      <Bar id="bar" :options="chartOptions" :data="barData" />
+    </div>
+    <div class="container">
     <SquirrelCard
       v-for="(squirrel, index) in squirrels"
       :key="squirrel.unique_squirrel_id"
@@ -8,18 +12,42 @@
       :index="index"
     />
   </div>
-  <Bar
-    id="chart"
-    :options="chartOptions"
-    :data="chartData"
-  />
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { Bar } from 'vue-chartjs'
+import { Bar, Radar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+} from 'chart.js'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+)
 import SquirrelCard from '@/components/SquirrelCard.vue'
 
+const radarData = ref({ labels: [], datasets: [] })
+const barData = ref({ labels: [], datasets: [] })
+const chartOptions = ref({ responsive: true })
 const squirrels = ref([])
 
 async function getSquirrels() {
@@ -29,6 +57,47 @@ async function getSquirrels() {
     )
     const data = await response.json()
     squirrels.value = data
+
+    const behaviors = ['running', 'chasing', 'climbing', 'eating', 'foraging']
+
+    const counts = {}
+    behaviors.forEach((b) => (counts[b] = 0))
+
+    data.forEach((s) => {
+      behaviors.forEach((b) => {
+        if (s[b] === true || s[b] === 'true') counts[b]++
+      })
+    })
+
+    radarData.value = {
+      labels: behaviors.map((b) => b.charAt(0).toUpperCase() + b.slice(1)),
+      datasets: [
+        {
+          label: 'Behavior frequency',
+          data: behaviors.map((b) => counts[b]),
+          backgroundColor: 'rgba(122, 80, 90, 0.4)',
+          borderColor: 'rgb(122, 80, 90)',
+          pointBackgroundColor: 'rgb(122, 80, 90)',
+        },
+      ],
+    }
+
+    const countColor = {}
+    data.forEach((squirrel) => {
+      const color = squirrel.primary_fur_color || 'Unknown'
+      countColor[color] = (countColor[color] || 0) + 1
+    })
+
+    barData.value = {
+      labels: Object.keys(countColor),
+      datasets: [
+        {
+          label: 'Most Common Fur Coat Colors in NYC Squirrels',
+          data: Object.values(countColor),
+          backgroundColor: ['#D3D3D3', '#808080', '#8B4513', '#000000'],
+        },
+      ],
+    }
   } catch (error) {
     console.log(error)
   }
@@ -39,7 +108,7 @@ onMounted(getSquirrels)
 <style scoped>
 .container {
   width: 80vw;
-  height: 80vw;
+  min-height: 80vw;
   margin: 30px auto;
   display: flex;
   flex-wrap: wrap;
@@ -53,5 +122,9 @@ h1 {
   width: 100%;
   font-style: italic;
   text-align: center;
+}
+.chart-wrapper {
+  width: 800px;
+  margin: 40px auto;
 }
 </style>
